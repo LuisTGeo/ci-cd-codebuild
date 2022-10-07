4ed3c2e582b02c6328d5bb068d646547968da8d1
luist@psma.com.au <qq97qf24ffL>
2022-10-07 18:33:39 +1100

    sss

---
+++ push.sh
@@ -0,0 +1,8 @@
+echo "---git add . ---"
+git add .
+
+echo "---git commit . ---"
+# git commit -m "last commit 12:23pm"
+git commit -m "$(read -p 'Commit description: ')"
+echo "---git push . ---"
+git push --set-upstream origin development
